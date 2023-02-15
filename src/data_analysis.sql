504f0e65065955f7332c505099eddf0e3692e25e
Arnar Már Kristinsson <70495459+Arnarmar0@users.noreply.github.com>
2023-02-15 14:24:09 +0000

    Create data_analysis.sql

---
+++ src/data_analysis.sql
@@ -0,0 +1,16 @@
+/* Skiluðu allar færslur sér yfir í SQL/Excel töfluna? */
+
+/* Veljum allar raðirnar */
+SELECT * FROM brautskraning;
+
+/* Teljum allar raðirnar */
+SELECT COUNT(*) FROM brautskraning;
+
+/* Summa allra brautskráðra nemenda úr HÍ */
+SELECT SUM(samtals) FROM brautskraning WHERE tegund_nams IS NULL;
+
+/* Summa allra brautskráðra nemenda eftir deildum */
+SELECT braut, SUM(samtals) FROM brautskraning GROUP BY braut;
+
+/* Summa allra brautskráðra nemenda eftir kynjum */
+SELECT SUM(kk) AS kk , SUM(kv) AS kvk FROM brautskraning WHERE tegund_nams is NULL;
