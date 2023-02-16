aead8c3bf0717927f016afa31a7259ccd1ff2cab
Arnar Már Kristinsson <70495459+Arnarmar0@users.noreply.github.com>
2023-02-16 18:48:59 +0000

    SQL data analysis almost complete

--- src/Creat_database.sql
+++ src/Creat_database.sql
@@ -7,15 +7,17 @@ CREATE TABLE test (id INT NOT NULL PRIMARY KEY, name VARCHAR(20) NOT NULL);
 INSERT INTO test VALUES (1, 'bob');
 
 /* Now we can use the table */
+CREATE TABLE brautskraning (id serial PRIMARY KEY, Year integer, Braut varchar, tegund_nams varchar, kk integer, kv integer, samtals integer);
 CREATE TABLE skraning (id serial PRIMARY KEY, Year integer, Braut varchar, tegund_nams varchar, kk integer, kv integer, samtals integer);
 SELECT * FROM brautskraning;
 SELECT * FROM skraning;
 
 select Year, Braut, samtals from brautskraning where tegund_nams is null 
-COPY brautskraning 
+COPY brautskraning;
 DROP TABLE brautskraning;
 COPY brautskraning TO 'C:\Users\hinri\OneDrive\Documents\HR\Viðskiptagreind\vidskiptagreind-hopverkefni-1\data\test.csv' WITH DELIMITER ',' CSV HEADER;
 
 /* To delete the table */
 
-DROP TABLE test;
+DROP TABLE brautskraning;
+DROP TABLE skraning;
--- src/data_analysis.sql
+++ src/data_analysis.sql
@@ -1,18 +1,90 @@
 /* Skiluðu allar færslur sér yfir í SQL/Excel töfluna? */
 
-/* Veljum allar raðirnar */
+/* Veljum allar raðirnar í brautskráningartöflunni */
 SELECT * FROM brautskraning;
 
-/* Teljum allar raðirnar */
+/* Veljum allar raðirnar í skráningartöflunni */
+SELECT * FROM skraning;
+
+/* Teljum allar raðirnar í brautskráningartöflunni */
 SELECT COUNT(*) FROM brautskraning;
+
+/* Teljum allar raðirnar í skráningartöflunni */
+SELECT COUNT(*) FROM skraning;
 
 /* Summa allra brautskráðra nemenda úr HÍ */
 SELECT SUM(samtals) FROM brautskraning WHERE tegund_nams IS NULL;
 SELECT SUM(samtals) FROM skraning WHERE tegund_nams IS NULL;
 
 
+/* Summa allra skráðra nemenda í HÍ eftir árum */
+SELECT Year, SUM(samtals) as skradir
+FROM skraning
+WHERE tegund_nams IS NULL
+GROUP BY Year
+ORDER BY Year;
+
+/* Summa allra brautskráðra nemenda í HÍ eftir árum */
+SELECT Year, SUM(samtals) as brautskradir
+FROM brautskraning
+WHERE tegund_nams IS NULL
+GROUP BY Year
+ORDER BY Year;
+
+/* Samtals allra skráðra og brautskráðra nemenda úr HÍ eftir árum með útskriftarprósentum */
+SELECT s.Year, s.skradir, b.brautskradir, ROUND((b.brautskradir * 100.0 / s.skradir)::numeric, 2) as utskriftarprosenta
+FROM
+  (SELECT Year, SUM(samtals) as skradir
+   FROM skraning
+   WHERE tegund_nams IS NULL
+   GROUP BY Year) s
+LEFT JOIN
+  (SELECT Year, SUM(samtals) as brautskradir
+   FROM brautskraning
+   WHERE tegund_nams IS NULL
+   GROUP BY Year) b
+ON s.Year = b.Year
+ORDER BY s.Year;
+
+/* Summa allra skráðra nemenda eftir deildum */
+SELECT braut, SUM(samtals) FROM skraning WHERE tegund_nams IS NULL GROUP BY braut;
+
 /* Summa allra brautskráðra nemenda eftir deildum */
 SELECT braut, SUM(samtals) FROM brautskraning WHERE tegund_nams IS NULL GROUP BY braut;
 
+/* Summa allra skráðra og brautskráðra nemenda eftir deildum með útskriftarprósentu */
+SELECT s.braut, s.skradir, b.brautskradir, ROUND((b.brautskradir * 100.0 / s.skradir)::numeric, 2) as utskriftarprosenta
+FROM
+  (SELECT braut, SUM(samtals) as skradir
+   FROM skraning
+   WHERE tegund_nams IS NULL
+   GROUP BY braut) s
+LEFT JOIN
+    (SELECT braut, SUM(samtals) as brautskradir
+     FROM brautskraning
+     WHERE tegund_nams IS NULL
+     GROUP BY braut) b
+ON s.braut = b.braut
+ORDER BY s.braut;
+
+/* Summa allra skráðra nemenda eftir kynjum */
+SELECT SUM(kk) AS kk , SUM(kv) AS kvk FROM skraning WHERE tegund_nams is NULL;
+
 /* Summa allra brautskráðra nemenda eftir kynjum */
 SELECT SUM(kk) AS kk , SUM(kv) AS kvk FROM brautskraning WHERE tegund_nams is NULL;
+
+/* Summa allra brautskráðra og skráðra nemenda eftir kynjum í einni töflu með útskriftarprósentum fyrir kk og kvk */
+SELECT s.Year, s.kk_signups, s.kvk_signups, b.kk_graduates, b.kvk_graduates, ROUND((b.kk_graduates * 100.0 / s.kk_signups)::numeric, 2) as utskriftarprosenta_kk, ROUND((b.kvk_graduates * 100.0 / s.kvk_signups)::numeric, 2) as utskriftarprosenta_kvk
+FROM
+  (SELECT Year, SUM(kk) as kk_signups, SUM(kv) as kvk_signups
+   FROM skraning
+   WHERE tegund_nams IS NULL
+   GROUP BY Year) s
+LEFT JOIN
+  (SELECT Year, SUM(kk) as kk_graduates, SUM(kv) as kvk_graduates
+   FROM brautskraning
+   WHERE tegund_nams IS NULL
+   GROUP BY Year) b
+ON s.Year = b.Year
+ORDER BY s.Year;
+
