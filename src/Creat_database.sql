59bab4d87388a73f1404ff34113f5cbaa09c8630
hinrik <hinrik20@ru.is>
2023-02-16 09:14:10 +0000

    some fixis

--- src/Creat_database.sql
+++ src/Creat_database.sql
@@ -7,9 +7,14 @@ CREATE TABLE test (id INT NOT NULL PRIMARY KEY, name VARCHAR(20) NOT NULL);
 INSERT INTO test VALUES (1, 'bob');
 
 /* Now we can use the table */
-CREATE TABLE brautskraning (id serial PRIMARY KEY, Year integer, Braut varchar, tegund_nams varchar, kk integer, kv integer, samtals integer);
+CREATE TABLE skraning (id serial PRIMARY KEY, Year integer, Braut varchar, tegund_nams varchar, kk integer, kv integer, samtals integer);
 SELECT * FROM brautskraning;
+SELECT * FROM skraning;
+
+select Year, Braut, samtals from brautskraning where tegund_nams is null 
+COPY brautskraning 
 DROP TABLE brautskraning;
+COPY brautskraning TO 'C:\Users\hinri\OneDrive\Documents\HR\Viðskiptagreind\vidskiptagreind-hopverkefni-1\data\test.csv' WITH DELIMITER ',' CSV HEADER;
 
 /* To delete the table */
 
